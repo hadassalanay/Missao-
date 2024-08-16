679f4366fee91e0c8a1d860f9804d793e0a98b1d
hadassalanay <112710540+hadassalanay@users.noreply.github.com>
2024-08-16 11:39:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+const caixaPrincipal = document.querySelector(".caixa-principla")
+const caixaPerguta = document.querySelector(".caixa-perguntal")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa--resultado")
+const textoresultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+enunciado:"pergunta 1 ",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+
+]
+},
+{
+    enunciado:"pergunta 2 ",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    
+    ]
+    },
+    {
+        enunciado:"pergunta 3 ",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        
+        ]
+        },
+        {
+            enunciado:"pergunta 4 ",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            
+            ]
+            },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
